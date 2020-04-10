85c450ed9db57c259685443559258c567d34924e
Maren de Vries <maren.de.vries@student.nhlstenden.com>
2020-04-10 13:38:28 +0200

    second

--- BestaandeJoin.php
+++ BestaandeJoin.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-
 <html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="stylesheet.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <head>
-<title>Hostmenu</title>
+<title>Page Title</title>
 </head>
 <body>
-
 <div class="container">
-  <div><button class="Knop" onClick="document.location.href='NewJoin.php'">Nieuwe speler</button> </div>
-    <div><button class="Knop" onClick="document.location.href='BestaandeJoin.php'">Bestaand speler</button> </div>
-    <div><button class="Knop" onClick="document.location.href='Hoofdmenu.php'">Terug</button></div>
-</div>
+  <div class="card" style="width: 60rem;">
+    <div class="card-body">
+    <form>
+          <p class="namespace">Zoek je naam in de lijst:</p>
+          
+          
+        </form><br>
+    <div>
+    <button class="Knop" name="Join" type="submit" onClick="document.location.href='speleroverzicht.php'"> Join </button> 
+    </div>
+  </div>
+    </div>
+    </div>
 </body>
 </html>
