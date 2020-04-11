a059094d80d522589d6231be095e70415d0c1c06
Willy Ji <willy_ji6@hotmail.com>
2020-04-11 14:32:16 +0200

    merge hostname into createhost added func

--- createhost.php
+++ createhost.php
@@ -1,5 +1,69 @@
+<?php
+
+session_start();
+include "config.php";
+require "user.php";
+
+$input_err = $input = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty(trim($_POST["hostname"]))) {
+        $input_err = "Please enter name.";
+    } else {
+
+        $input = trim(str_replace(array("'", "'"), '', $_POST['hostname']));
+
+        //CHECK IF NAME ALREADY EXISTS IN DATABASE
+        $already_exists = false;
+        $sql = "SELECT name FROM pokerDb.player_data WHERE name = :input";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':input', $input);
+        $stmt->execute();
+        if($stmt->rowCount() > 0){
+            $already_exists = true;
+            $input_err = "Name already exists.";
+        }
+        else{
+            //SET PLAYER DATA IN DB
+            $sql = "INSERT INTO pokerDb.player_data (name) VALUES (?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$input]);
+            //GET HOST PLAYER ID
+            $sql = "SELECT player_id FROM pokerDb.player_data WHERE name='$input'";
+            $stmt= $pdo->query($sql);
+            $result2 = $stmt->fetch();
+
+            //SET SESSION DATA IN DB
+            $sql = "INSERT INTO pokerDb.session_game (host_name) VALUES (?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$input]);
+            //GET SESSION ID
+            $sql = "SELECT MAX(session_id) FROM pokerDb.session_game";
+            $stmt= $pdo->query($sql);
+            $result1 = $stmt->fetch();
+
+            //SET PLAYER GAME FOR HOST
+            $sql = "INSERT INTO pokerDb.player_game (game_id, player_id_fk) VALUES ($result1[0],$result2[0])";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute();
+
+            //STORE ID's IN SESSION
+            $_SESSION['session_game_id'] = $result1[0];
+            $_SESSION['player_id'] = $result2[0];
+            $_SESSION['is_host'] = true;
+            $_SESSION['settings_id_set'] = false;
+
+            // Redirect user to hostoverzicht page
+            header("location: hostoverzicht.php");
+            exit;
+        }
+    }
+}
+?>
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,17 +73,21 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <div class="card-group">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Nieuwe hostnaam aanmaken</h5>
-        <form id="newhostfield">
-            <p>Voer hier een nieuwe hostnaam in</p>
-            <input name=newhostfield type="search" required />
-            <input id="submitblind" value="Aanmaken" type="submit" />
-        </form><br>    
-        <a href="hostmenu.php"><button class=back>Terug</button></a>
-      </div>
+<div class="container">
+    <div class="card-group">
+        <div class="card">
+            <div class="card-body">
+             <h5 class="card-title">Nieuwe hostnaam aanmaken</h5>
+                <form class="edit2" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                     <p>Voer hier een nieuwe hostnaam in</p>
+                     <input name=hostname type="text" required /><br>
+                     <input class="back" value="Aanmaken" type="submit" />
+                </form><br>
+             <a href="hostmenu.php"><button class=back>Terug</button></a>
+         </div>
+        </div>
     </div>
 </div>
+<!-- Masthead -->
 </body>
+</html>
