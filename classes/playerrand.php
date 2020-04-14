98373dcdc1e8807acaea673094bdd54ab61d778f
Willy Ji <willy_ji6@hotmail.com>
2020-04-14 21:22:51 +0200

    real random

--- classes/playerrand.php
+++ classes/playerrand.php
@@ -12,39 +12,238 @@ $numberOfTablesNeeded = ceil(count($allPlayers) / 6);
 
 $t1 = $t2 = $t3 = $t4 = $t5 = 0;
 
-for($increment = 0; $increment < count($allPlayers); $increment++){
-    $rand1 = rand(1, $numberOfTablesNeeded);
-    if($rand1 == 1 && $t1 < 6){
-        $seat_random[$increment] = $rand1;
-        $t1++;
-    }elseif($rand1 == 1 && $t1 >= 6){
-        $increment--;
+
+if(count($allPlayers) % 6 == 0){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 6){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 6){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 6){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 6){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 6){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 6){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 6){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 6){
+            $increment--;
+        }
+        if($rand1 == 5 && $t5 < 6){
+            $seat_random[$increment] = $rand1;
+            $t5++;
+        }elseif($rand1 == 5 && $t5 >= 6){
+            $increment--;
+        }
     }
-    if($rand1 == 2 && $t2 < 6){
-        $seat_random[$increment] = $rand1;
-        $t2++;
-    }elseif($rand1 == 2 && $t2 >= 6){
-        $increment--;
+}elseif(count($allPlayers) % 5 == 0){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 5){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 5){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 5){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 5){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 5){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 5){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 5){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 5){
+            $increment--;
+        }
+        if($rand1 == 5 && $t5 < 5){
+            $seat_random[$increment] = $rand1;
+            $t5++;
+        }elseif($rand1 == 5 && $t5 >= 5){
+            $increment--;
+        }
     }
-    if($rand1 == 3 && $t3 < 6){
-        $seat_random[$increment] = $rand1;
-        $t3++;
-    }elseif($rand1 == 3 && $t3 >= 6){
-        $increment--;
+}elseif(count($allPlayers) % 5 == 1 && count($allPlayers) != 6){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 6){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 6){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 5){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 5){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 5){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 5){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 5){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 5){
+            $increment--;
+        }
+        if($rand1 == 5 && $t5 < 5){
+            $seat_random[$increment] = $rand1;
+            $t5++;
+        }elseif($rand1 == 5 && $t5 >= 5){
+            $increment--;
+        }
     }
-    if($rand1 == 4 && $t4 < 6){
-        $seat_random[$increment] = $rand1;
-        $t4++;
-    }elseif($rand1 == 4 && $t4 >= 6){
-        $increment--;
+}elseif(count($allPlayers) % 5 == 2 && count($allPlayers) != 7){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 6){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 6){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 6){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 6){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 5){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 5){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 5){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 5){
+            $increment--;
+        }
+        if($rand1 == 5 && $t5 < 5){
+            $seat_random[$increment] = $rand1;
+            $t5++;
+        }elseif($rand1 == 5 && $t5 >= 5){
+            $increment--;
+        }
     }
-    if($rand1 == 5 && $t5 < 6){
-        $seat_random[$increment] = $rand1;
-        $t5++;
-    }elseif($rand1 == 5 && $t5 >= 6){
-        $increment--;
+}elseif(count($allPlayers) % 5 == 3 && count($allPlayers) % 6 != 0 && count($allPlayers) != 8){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 6){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 6){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 6){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 6){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 6){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 6){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 5){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 5){
+            $increment--;
+        }
+        if($rand1 == 5 && $t5 < 5){
+            $seat_random[$increment] = $rand1;
+            $t5++;
+        }elseif($rand1 == 5 && $t5 >= 5){
+            $increment--;
+        }
     }
-}
+}elseif(count($allPlayers) % 5 == 4 && count($allPlayers) % 6 != 0 && count($allPlayers) != 29){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 5){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 5){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 4){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 4){
+            $increment--;
+        }
+        if($rand1 == 3 && $t3 < 5){
+            $seat_random[$increment] = $rand1;
+            $t3++;
+        }elseif($rand1 == 3 && $t3 >= 5){
+            $increment--;
+        }
+        if($rand1 == 4 && $t4 < 5){
+            $seat_random[$increment] = $rand1;
+            $t4++;
+        }elseif($rand1 == 4 && $t4 >= 5){
+            $increment--;
+        }
+    }
+}elseif(count($allPlayers) == 7){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 4){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 4){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 3){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 3){
+            $increment--;
+        }
+    }
+}elseif(count($allPlayers) == 8){
+    for($increment = 0; $increment < count($allPlayers); $increment++){
+        $rand1 = rand(1, $numberOfTablesNeeded);
+        if($rand1 == 1 && $t1 < 4){
+            $seat_random[$increment] = $rand1;
+            $t1++;
+        }elseif($rand1 == 1 && $t1 >= 4){
+            $increment--;
+        }
+        if($rand1 == 2 && $t2 < 4){
+            $seat_random[$increment] = $rand1;
+            $t2++;
+        }elseif($rand1 == 2 && $t2 >= 4){
+            $increment--;
+        }}}
+
+
 
 for($player = 0; $player < count($allPlayers); $player++){
     $fk = $allPlayers[$player]['player_id_fk'];
@@ -54,69 +253,3 @@ for($player = 0; $player < count($allPlayers); $player++){
     $stmt->bindValue(':session_id', $_SESSION['session_game_id']);
     $stmt->execute();
 }
-
-//for ($table_id = 1; $table_id <= $numberOfTablesNeeded ; $table_id++){
-//
-//    for($seat_id = 1; $seat_id <= 6; $seat_id++){
-//
-//        $table_increment = $table_id * 6 - 7;
-//        if(($table_increment+$seat_id) < count($allPlayers)) {
-//            $fk = $allPlayers[($table_increment + $seat_id)]['player_id_fk'];
-//            $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = :table_id WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//            $stmt2 = $pdo->prepare($query);
-//            $stmt2->bindValue(':table_id', $table_id);
-//            $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//            $stmt2->execute();
-//        }
-//    }
-//}
-
-//        if(($table_id*$seat_id)-1 < count($allPlayers)) {
-//            $table_increment = $table_id * 6 - 7;
-//            $fk = $allPlayers[(-1 + $seat_id)]['player_id_fk'];
-//            $fk2 = $allPlayers[(5 + $seat_id)]['player_id_fk'];
-//            $fk3 = $allPlayers[(11 + $seat_id)]['player_id_fk'];
-//            $fk4 = $allPlayers[(17 + $seat_id)]['player_id_fk'];
-//            $fk5 = $allPlayers[(23 + $seat_id)]['player_id_fk'];
-//            $fk = $allPlayers[($table_increment + $seat_id)]['player_id_fk'];
-//            $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = 1 WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//            $stmt2 = $pdo->prepare($query);
-//            $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//            $stmt2->execute();
-//        }
-//
-//for ($i = 0; $i < (count($allPlayers)); $i++) {
-//    $table_id = floor($i / 6);
-//    $fk = $allPlayers[$i]['player_id_fk'];
-//    $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = '{$table_id}' WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//    $stmt2 = $pdo->prepare($query);
-//    $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//    $stmt2->execute();
-//}
-
-//if(($table_id*$seat_id)-1 < count($allPlayers)) {
-//    $fk = $allPlayers[($table_id*$seat_id)-1]['player_id_fk'];
-//    $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = 1 WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//    $stmt2 = $pdo->prepare($query);
-//    $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//    $stmt2->execute();
-//}
-
-//if($table_id == 1){
-//    if(($table_id*$seat_id)-1 < count($allPlayers)) {
-//        $fk = $allPlayers[($table_id*$seat_id)-1]['player_id_fk'];
-//        $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = 1 WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//        $stmt2 = $pdo->prepare($query);
-//        $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//        $stmt2->execute();
-//    }
-//}
-//if($table_id == 2){
-//    if(($table_id*$seat_id)-1 < count($allPlayers)) {
-//        $fk = $allPlayers[$seat_id+5]['player_id_fk'];
-//        $query = "UPDATE pokerDb.player_game SET pokerDb.player_game.tafel = 2 WHERE pokerDb.player_game.player_id_fk = '{$fk}' AND pokerDb.player_game.game_id = :session_id";
-//        $stmt2 = $pdo->prepare($query);
-//        $stmt2->bindValue(':session_id', $_SESSION['session_game_id']);
-//        $stmt2->execute();
-//    }
-//}
